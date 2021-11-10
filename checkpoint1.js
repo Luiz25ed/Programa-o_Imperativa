d91a3828f62f5ebef79afa0291915c91d70634f9
Luiz Felipe <lfelippi.seg@gmail.com>
2021-11-09 22:14:23 -0300

    Subindo checkpoint para repositorio git hub

---
+++ checkpoint1.js
@@ -0,0 +1,56 @@
+//Olá, você foi contratado para executar este projeto. É importante que você analise, entenda o pedido do cliente e desenvolva conforme solicitado. Veja abaixo os requisitos do projeto:
+
+//- Precisamos desenvolver um menu para um microondas super veloz, onde teremos 5 opções de comida com seus respectivos tempos pré-definidos. 
+
+     // 1 - Pipoca – 10 segundos (padrão);
+      //2 - Macarrão – 8 segundos (padrão);
+      //3 - Carne – 15 segundos (padrão);
+     // 4 - Feijão – 12 segundos (padrão);
+     // 5 - Brigadeiro – 8 segundos (padrão); 
+
+//- O usuário poderá alterar o tempo padrão, aumentando ou diminuindo de acordo com sua vontade. Se o tempo informado for maior que 2x o necessário, exibir mensagem que a comida queimou.
+//- Se o tempo for menor que o padrão, exibir a mensagem: "tempo insuficiente"; 
+//- Opções não listadas no menu, devem exibir uma mensagem de erro: "Prato inexistente";
+//- Se o tempo for 3x maior que o necessário para o prato, o microondas deve exibir a mensagem: “kabumm”;
+//- No final de cada tarefa, o microondas deverá exibir a mensagem: "Prato pronto, bom apetite!!!"
+if (true) {
+    let menuMicroondas = [
+        "Pipoca",
+        "Macarrão",
+        "Carne",
+        "Feijão",
+        "Brigadeiro" ];
+    
+    
+        let listaVazia = [
+         "Pipoca - 10 segundos",
+         "Macarrão - 8 segundos",
+         "Carne - 15 segundos",
+         " Feijão - 12 segundos",
+         "Brigadeiro - 8 segundos"];
+    let listaNumero = [1, 2, 3, 4, 5,];
+    
+    let tempoPadrão = "Tempo insuficiente";
+    
+    let listaMenu = undefined;
+    listaMenu = "Prato inexistente";
+}
+ 
+
+let menuLista = {
+    Pipoca = 10,
+    Macarrão = 8,
+    Carne = 15, 
+    Feijão = 12,
+    Brigadeiro = 8,
+
+
+}
+
+
+
+ 
+
+    
+    
+    
